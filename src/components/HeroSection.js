bf47c6f9c9bf6d9c4e893e0d349e542fa33d41b6
oraweechan <89260194+oraweechan@users.noreply.github.com>
2022-03-01 14:13:52 -0800

    added Hero and NavMenu component

---
+++ src/components/HeroSection.js
@@ -0,0 +1,7 @@
+export default function HeroSection() {
+    return (
+        <div className= "hero-container">
+            <h1>Hero Section</h1>
+        </div>
+    )
+}; 
---
+++ src/components/NavMenu.js
@@ -0,0 +1,18 @@
+export default function NavMenu() {
+    return (
+        <div className= "nav-menu-container">
+            <div className="nav-menu-logo">
+                <h3>Restaurant</h3>
+            </div>
+            <div className="nav-menu-items">
+                <ul>
+                    <li>About</li>
+                    <li>Gallery</li>
+                    <li>Testimonial</li>
+                    <li>Contact</li>
+                    <li>Reservation</li>
+                </ul>
+            </div>
+        </div>
+    )
+}; 
